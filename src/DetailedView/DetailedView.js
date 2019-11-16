1e25ac7d07a28c40fbaf96b3e29bea0f196d078b
Patrik Jokela <patrik.jokela@gmail.com>
2019-11-16 21:36:37 +0200

    edited css

--- src/DetailedView/DetailedView.js
+++ src/DetailedView/DetailedView.js
@@ -7,11 +7,28 @@ class DetailedView extends Component {
   render() {
     return (
       <div className="DetailedView">
+<<<<<<< Updated upstream
         <h2>This is the Detailed View :D</h2>
         
         {/* We pass the model as property to the Sidebar component */}
         <Sidebar model={this.props.model}/>
 
+=======
+          <Link to="/">
+              <button>Go back</button>
+          </Link>
+          <h1> Item Name </h1>
+          <h2> How much did you waste? </h2>
+            {/*<Sidebar model={this.props.model}/>*/}
+            <Slider model={this.props.model} color="#ff6902"/>
+          {/*<div>{this.props.model.getSliderValue()} </div>*/}
+            <div class="total"> 
+                <p> You have thrown away euros worth of: </p>
+                <div class="total_value">   
+                    {this.state.sliderValue}
+                </div>
+            </div>
+>>>>>>> Stashed changes
       </div>
     );
   }
