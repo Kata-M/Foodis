62d3009a21cd03c96fb7aa2d92aee6eccdd6584a
patuuh <44578687+patuuh@users.noreply.github.com>
2019-11-16 16:51:04 +0200

    added post request

--- api.py
+++ api.py
@@ -7,46 +7,40 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask import jsonify
 
+mydb = mysql.connector.connect(
+            host="mysql-138a8d13-patrik-e2ce.aivencloud.com",
+            user="avnadmin",
+            database='defaultdb',
+            passwd="xlh62r1ha3vkniyb",
+            port="17083"
+            )
+
 app = Flask(__name__)
 api = Api(app)
 
 class Products(Resource):
     def get(self):
-        mydb = mysql.connector.connect(
-            host="mysql-138a8d13-patrik-e2ce.aivencloud.com",
-            user="avnadmin",
-            database='defaultdb',
-            passwd="xlh62r1ha3vkniyb",
-            port="17083"
-            )
         mycursor = mydb.cursor()
         query = mycursor.execute("select * from products")
         return jsonify(mycursor.fetchall())
 
+    def post(self, name, price):
+        mycursor = mydb.cursor()
+        sql = "INSERT INTO products (name, price) VALUES (%s, %s)" % (name, price)
+        mycursor.execute(sql)
+        mydb.commit()
+        return print('Done!')
+
 class Products_Name(Resource):
     def get(self, name):
-        conn = mysql.connector.connect(
-            host="mysql-138a8d13-patrik-e2ce.aivencloud.com",
-            user="avnadmin",
-            database='defaultdb',
-            passwd="xlh62r1ha3vkniyb",
-            port="17083"
-            )
-        mycursor = conn.cursor()
-        query = mycursor.execute("select price from products where name =%s ;" %name )
+        mycursor = mydb.cursor()
+        mycursor.execute("select price from products where name =%s ;" %name )
         return jsonify(mycursor.fetchall())
 
 class Products_ID(Resource):
     def get(self, id):
-        conn = mysql.connector.connect(
-            host="mysql-138a8d13-patrik-e2ce.aivencloud.com",
-            user="avnadmin",
-            database='defaultdb',
-            passwd="xlh62r1ha3vkniyb",
-            port="17083"
-            )
-        mycursor = conn.cursor()
-        query = mycursor.execute("select price from products where id =%d ;" %int(id) )
+        mycursor = mydb.cursor()
+        mycursor.execute("select price from products where id =%d ;" %int(id) )
         return jsonify(mycursor.fetchall())
         
 
