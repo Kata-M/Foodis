898a2e0517f23edeff35ead6bc4190398a9ff3bb
Patrik Jokela <patrik.jokela@gmail.com>
2019-11-17 04:42:11 +0200

    Update DataModel.js

--- src/data/DataModel.js
+++ src/data/DataModel.js
@@ -22,7 +22,7 @@ const DataModel = function () {
   }
 
   var testFood1 = new FoodItem(0,"Milk", 1.5, "3",true,false,0, 9);
-  var testFood2 = new FoodItem(1,"Bread", 1.05, "1",false,false,0, 4);
+  var testFood2 = new FoodItem(1,"Bread", 4.80, "1",false,false,0, 4);
   var testFood3 = new FoodItem(2,"Cucumber", .07, "3",true,false,0, 2);
   var testFood4 = new FoodItem(3,"Chocolate", 1.25, "2",false,false,0, 1);
   var testFood5 = new FoodItem(4,"Mango", 3.5, "100",false,false,0, 3);
